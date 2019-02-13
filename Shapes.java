c6eeb057de66f7491e81968ab790b1e60fe40138
William Herman <William@Williams-MacBook-Air.local>
2019-02-12 19:30:28 -0500

    shapes

---
+++ Shapes.java
@@ -0,0 +1,44 @@
+package Assignment1;
+public class Shapes {
+    public static void main(String[] args) {
+        System.out.println("************");
+        System.out.println("*           *");
+        System.out.println("*           *");
+        System.out.println("*           *");
+        System.out.println("*           *");
+        System.out.println("*           *");
+        System.out.println("*           *");
+        System.out.println("*           *");
+        System.out.println("************");
+        System.out.println(" ");
+        System.out.println("   ***   ");
+        System.out.println("  *    *  ");
+        System.out.println(" *      *");
+        System.out.println("*        *");
+        System.out.println(" *      *");
+        System.out.println("  *    *  ");
+        System.out.println("   ***   ");
+        System.out.println(" ");
+        System.out.println("   *   ");
+        System.out.println("  *** ");
+        System.out.println(" ***** ");
+        System.out.println(" *   *");
+        System.out.println(" *   *");
+        System.out.println(" *   *");
+        System.out.println(" *   *");
+        System.out.println(" *   *");
+        System.out.println(" *   *");
+        System.out.println(" ");
+        System.out.println("    *    ");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("  *****");
+        System.out.println("   ***");
+        System.out.println("    *    ");
+
+
+
+
+    }
+}
