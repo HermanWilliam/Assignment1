ca29fe33d36a6db8f925e29b49f61194fe68870c
William Herman <William@Williams-MacBook-Air.local>
2019-02-12 19:34:36 -0500

    Assignment 1

---
+++ Statements.java
@@ -0,0 +1,17 @@
+package Assignment1;
+import java.util.Scanner;
+public class Statements {
+    public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter an integer:");
+        int Integer = keyboard.nextInt();
+        int b = 0;
+        int c = 0;
+        int a = b*c;
+         // this program performs a sample payroll calculation
+
+
+
+
+    }
+}
